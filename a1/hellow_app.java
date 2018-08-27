359d2aa04e3014aca74b366d0458673c4c6a2981
kurtturner21 <kurtturner21@gmail.com>
2018-08-27 19:48:29 -0400

    Create hellow_app.java

---
+++ a1/hellow_app.java
@@ -0,0 +1,9 @@
+/*
+	CISS 301, Fall 2018
+	Hello world applicaton
+*/
+class helloworld {
+	public static void main(String args[]){
+		System.out.println("Hello world, my name is Mr. Turner");
+	}
+}
